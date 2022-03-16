e80d76f24d6121e8350d926379a1f17c38e08ce8
Qu-ya <liyunliou23@gmail.com>
2022-03-16 16:57:07 +0800

    加入專案檔案。

---
+++ 20220316-2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace _20220316_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            char op;   //只有一個字元(加減乘除)
+
+            Console.Write("Number1:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Operator(+, -, *, /):");
+            op = Convert.ToChar(Console.ReadLine());
+
+            switch(op)
+            {
+                case '+':
+                    Console.WriteLine("{0}", a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("{0}", a - b);
+                    break;
+                case '*':
+                    Console.WriteLine("{0}", a * b);
+                    break;
+                case '/':
+                    Console.WriteLine("{0}", Convert.ToDouble(a) / b);
+                    break;
+                default:
+                    Console.WriteLine("無法執行你要求的運算");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}*/
+
+
+
+namespace _20220316_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            double c=999D;
+            char op;   //只有一個字元(加減乘除)
+
+            Console.Write("Number1:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Operator(+, -, *, /):");
+            op = Convert.ToChar(Console.ReadLine());
+
+            switch (op)
+            {
+                case '+':
+                    c = Convert.ToDouble(a + b);
+                    break;
+                case '-':
+                    c = Convert.ToDouble(a - b);
+                    break;
+                case '*':
+                    c = Convert.ToDouble(a * b);
+                    break;
+                case '/':
+                    c = Convert.ToDouble(a) / b;
+                    break;
+                default:
+                    Console.WriteLine("無法執行你要求的運算");
+                    break;
+            }
+            if (c != 999D)
+                Console.WriteLine("計算結果是；{0}", c);
+            Console.ReadLine();
+        }
+    }
+}
